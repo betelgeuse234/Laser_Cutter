c50e8feefb2537211f05d2344ddc868710cbf017
betelgeuse234 <snyderna@mail.gvsu.edu>
2017-02-14 00:38:41 -0600

    Data sheets and comment block were added
    Added data sheets for I2C temp sensor, LCD, and started work on standard
    comment block

--- Software/MCU/MCU/Drivers/Source/timer.c
+++ Software/MCU/MCU/Drivers/Source/timer.c
@@ -1,6 +1,15 @@
 /*
- * timer.c
+ * @file timer.c
+ * Description goes here. 
+ * Shall we have the same comment header in the source and header files?
+ * This description shall just give the big picture for this file. 
+ * The individual functions shall have their own descriptions.
+ * 
+ * @author Nathan Snyder
+ * @e-mail snydernlyle@gmail.com 
+ * I like the idea of having contact information here in case anyone ever looks at this in the future and has questions.
  *
- * Created: 2/13/2017 1:45:32 AM
- *  Author: betel
+ * @version 1.0
+ *
+ * We should Include a license here somewhere probably
  */ 
